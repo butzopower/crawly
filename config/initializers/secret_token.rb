003b6b63a42e45157534037c03e8ec64198d1cec
Brian Butz <bbutz@bbutz.local>
2012-05-04 22:07:49 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crawly::Application.config.secret_token = '70d72d8bf2262e3c1aff7f827c2e5def3b1d4278a8fc2e5c8fdf52e59298f37dc294cc3c37da072bd07f7631ed822d741ba1cbda3cfa172b49e64fda4bf36d13'
