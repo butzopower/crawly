8d7b57cce607d19cb9f66082eded3cce36319839
Brian Butz <butzopower@gmail.com>
2012-05-06 21:07:47 -0700

    Admin layout basic

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,4 +1,5 @@
 class Admin::BaseController < ApplicationController
+  layout 'admin'
   # YAGNI, sure, but of course admin has auth
   # and it will be nice to have admin controllers
   # have a shared base controller when that time
